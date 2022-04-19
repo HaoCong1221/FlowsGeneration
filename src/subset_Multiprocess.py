aae1a1138447b67915f952decf4aedf2469df66f
HaoCong <haoco@student.chalmers.se>
2022-04-19 18:22:41 +0200

    Multiprocess_for different grids

--- src/subset_Multiprocess.py
+++ src/subset_Multiprocess.py
@@ -1,4 +1,4 @@
-# This is for 2km
+#This is for 1km,3km,4km,5km
 import multiprocessing
 import sys
 sys.path.append('../lib/')
@@ -16,20 +16,62 @@ from multiprocessing import Process, Manager
 import time
 
 
-def preprocess(grid_file):
+def preprocess(grid_file, zone_file):
     global population_density, geometry, big_within, bigzone_name
     grids = gpd.read_file(grid_file)
+    zones = gpd.read_file(zone_file)
+    
+    zones.loc[:, 'deso_3'] = zones.loc[:, 'deso'].apply(lambda x: x[:2])
+    zones_subset = zones.loc[zones['deso_3'] == '14', :]
+    zone_name = list(zones_subset['deso'])
 
-    big_within = defaultdict(list)
+    within = defaultdict(list)
+
+    #Filter out the grids which do not have the density data
+    deleteIndex = []
 
     for index, row in grids.iterrows():
-        big_within[row['deso_5']].append(row['zone'])
+        if row['deso'] == None or math.isnan(row['density']):
+            deleteIndex.append(index)
+        else:
+            within[row['deso']].append(row['zone'])
 
+    grids = grids.drop(index = deleteIndex)
 
     population_density = dict(zip(grids.zone, grids.density))
     geometry = dict(zip(grids.zone, grids.geometry))
 
-    bigzone_name = list(grids.deso_5.drop_duplicates())
+    bigzone_name = []
+    bigCover = []
+    subCover = []
+    old_name = zone_name[0][0:5]
+
+    bigzone_name.append(old_name)
+    subCover.extend(within[zone_name[0]])
+
+    for i in range(1, len(zone_name)):
+        new_name = zone_name[i][0:5]
+
+        if new_name == old_name:
+        # this two zones belong the same big zone
+            subCover.extend(within[zone_name[i]])
+
+        if new_name != old_name:
+            # find a new big zone
+            #store old results
+            bigCover.append(deepcopy(subCover))
+            subCover.clear()
+
+            #store new resutls
+            bigzone_name.append(new_name)
+            subCover.extend(within[zone_name[i]])
+
+        old_name = new_name
+
+    # handle the lastest case
+    bigCover.append(subCover)
+    big_within = dict(zip(bigzone_name, bigCover))
+
 
 def ODM_zone_level(index, area, begin_index, end_index, bigzone_name, population_density, geometry, big_within, model_output):
     r_average = area/math.pi
@@ -54,7 +96,7 @@ def ODM_zone_level(index, area, begin_index, end_index, bigzone_name, population
                     
                         distance = deltax * deltax + deltay * deltay
 
-                        tmp =  area * r_average / distance * parameter
+                        tmp =  area * r_average  / distance * parameter
                         
                         average_daily_trips = average_daily_trips + tmp * ( population_density[big_within[bigzone_name[i]][begin]] + population_density[big_within[bigzone_name[j]][end]] )
             element[bigzone_name[j]] = average_daily_trips
@@ -75,12 +117,13 @@ if __name__ == '__main__':
     global bigzone_name, population_density, geometry, big_within
     totalProcess = 4
 
-    area = 4
-    gpd_file = '../results/grids_vgr_2km_density_deso5.shp'
-    results_output = "../results/model_output_2km.txt"
+    area = 9
+    gpd_file = '../results/grids_vgr_3km_density_deso.shp'
+    zone_file = '../dbs/sweden/zones/DeSO/DeSO_2018_v2.shp'
+    results_output = "../results/model_output_3km.txt"
     
 
-    preprocess(gpd_file)
+    preprocess(gpd_file, zone_file)
     print("Start computing.....")
     startTime = time.time()
     processes = []
@@ -105,4 +148,3 @@ if __name__ == '__main__':
             p.join()
         print("Finish! Total time: {} s.".format(time.time() - startTime))
         store(results_output, model_output)
-    
