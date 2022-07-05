0db8a3e9df6f0af8d5f4ae5b13dcfec1df550794
HaoCong <haoco@student.chalmers.se>
2022-07-05 22:21:24 +0200

    ssps indicators updated

--- src/ssps.py
+++ src/ssps.py
@@ -25,8 +25,10 @@ def preprocess(grid_file):
 
     grids.loc[:, 'Radius'] = grids.loc[:, "Area"].apply(lambda x : math.sqrt(x / math.pi))
     grids.loc[:, 'miu'] = 0
+    total_poplution = 0
     for i in grids.index:
         grids.loc[i, 'miu'] = get_miu(density=grids.loc[i, 'pop'], r=grids.loc[i, 'Radius'], f_home=1)
+        total_poplution = total_poplution + grids.loc[i, 'pop'] * grids.loc[i, 'Area']
 
     point_x_info = np.zeros(shape=len(name_list))
     point_y_info = np.zeros(shape=len(name_list))
@@ -58,16 +60,15 @@ def preprocess(grid_file):
     del geometry
     gc.collect()
 
-    return area, miu, parameter, sp_data_sweden_x, sp_data_sweden_y, sp_data_simulation_x, sp_data_simulation_y, point_x_info, point_y_info
+    return area, miu, parameter, sp_data_sweden_x, sp_data_sweden_y, sp_data_simulation_x, sp_data_simulation_y, point_x_info, point_y_info, total_poplution
 
 
 @jit(nopython=True, parallel=True)
 def computing(area, miu, parameter, sp_data_sweden_x, sp_data_sweden_y, sp_data_simulation_x, sp_data_simulation_y, point_x_info, point_y_info):
     flows = 0
-    travel_demand = 0
-    travel_demand_D_survey = 0
-    travel_demand_D_simulation = 0
-    total_distance = 0
+    flows_d = 0
+    flows_D_survey = 0
+    flows_D_simulation = 0
     for i in numba.prange(0, len(area)):    
         for j in range(0, len(area)): 
             if i != j:
@@ -77,37 +78,96 @@ def computing(area, miu, parameter, sp_data_sweden_x, sp_data_sweden_y, sp_data_
                 distance = math.sqrt(distance_square)
 
                 tmp = miu[j] * area[i] / distance_square * parameter
-                flows = flows + tmp * distance
-                travel_demand = travel_demand + tmp * distance * distance
-                travel_demand_D_survey = travel_demand_D_survey  + tmp * np.interp(distance, sp_data_sweden_x, sp_data_sweden_y) * distance
-                travel_demand_D_simulation = travel_demand_D_simulation + tmp * np.interp(distance, sp_data_simulation_x, sp_data_simulation_y) * distance
-                total_distance = total_distance + distance
-    average_distance = total_distance / ((len(area) - 1) * (len(area) - 1))
-    return flows, travel_demand, travel_demand_D_survey, travel_demand_D_simulation, average_distance
-
+                
+                flows = flows + tmp 
+                flows_d = flows_d + tmp * distance
+                flows_D_survey = flows_D_survey + tmp * np.interp(distance, sp_data_sweden_x, sp_data_sweden_y) * distance
+                flows_D_simulation = flows_D_simulation  + tmp * np.interp(distance, sp_data_simulation_x, sp_data_simulation_y) * distance
+                
+                
+    return flows, flows_d, flows_D_survey, flows_D_simulation
 
 if __name__ == "__main__":  
-    target_name = ["ssp1_yr2010", "ssp1_yr2020", "ssp1_yr2030", "ssp1_yr2040", "ssp1_yr2050", "ssp1_yr2060", "ssp1_yr2070", "ssp1_yr2080", "ssp1_yr2090", "ssp1_yr2100"]
+    target_ssp = 'ssp1_{}'
+    target_name = ["yr2010", "yr2020", "yr2030", "yr2040", "yr2050", "yr2060", "yr2070", "yr2080", "yr2090", "yr2100"]
+    poplution = []
+    
+    total_flows = []
+    total_travel_demand_d = []
+    total_travel_demand_D_survey = []
+    total_travel_demand_D_simulation = []
+    
+    total_flows_p = []
+    total_travel_demand_d_p = []
+    total_travel_demand_D_survey_p = []
+    total_travel_demand_D_simulation_p = []
+
     for name in target_name:
-        print(name)
-        grid_file = '../ssps/sweden_vg_{}.shp'.format(name)
-        area, miu, parameter, sp_data_sweden_x, sp_data_sweden_y, sp_data_simulation_x, sp_data_simulation_y, point_x_info, point_y_info = preprocess(grid_file)
-        print("Start computing {}".format(name))
+        file_name = target_ssp.format(name)
+        print(file_name)
+        grid_file = '../results/ssps/ssps/sweden_vg_{}.shp'.format(file_name)
+        area, miu, parameter, sp_data_sweden_x, sp_data_sweden_y, sp_data_simulation_x, sp_data_simulation_y, point_x_info, point_y_info, total_popultion = preprocess(grid_file)
+        print("Start computing {}".format(file_name))
         startTime = time.time()
     
-        flows, travel_demand, travel_demand_D_survey, travel_demand_D_simulation, average_distance = computing(area, miu, parameter, sp_data_sweden_x, sp_data_sweden_y, sp_data_simulation_x, sp_data_simulation_y, point_x_info, point_y_info)
+        flows, flows_d, flows_D_survey, flows_D_simulation = computing(area, miu, parameter, sp_data_sweden_x, sp_data_sweden_y, sp_data_simulation_x, sp_data_simulation_y, point_x_info, point_y_info)
 
-        result_file = "../results/{}_result.txt".format(name)
+        flows_p = flows / total_popultion
+        flows_d_p = flows_d / total_popultion
+        flows_D_survey_p = flows_D_survey / total_popultion
+        flows_D_simulation_p = flows_D_simulation / total_popultion
+
+
+        poplution.append(total_popultion)
+        total_flows.append(flows)
+        total_travel_demand_d.append(flows_d)
+        total_travel_demand_D_survey.append(flows_D_survey)
+        total_travel_demand_D_simulation.append(flows_D_simulation)
+        total_flows_p.append(flows_p)
+        total_travel_demand_d_p.append(flows_d_p)
+        total_travel_demand_D_survey_p.append(flows_D_survey_p)
+        total_travel_demand_D_simulation_p.append(flows_D_simulation_p)
+
+
+        result_file = "../results/{}_result.txt".format(file_name)
         with open(result_file, 'w') as f:
-            f.write("Distance based flows: {}\n".format(flows))
-            f.write("Distance based travel_demand_d: {}\n".format(travel_demand))
-            f.write('Distance based travel_demand_D_survey: {}\n'.format(travel_demand_D_survey))
-            f.write("Distance based travel_demand_D_simulation: {}\n".format(travel_demand_D_simulation))
-            f.write("-----\n")
-            f.write("Distance weighted flows: {}\n".format(flows / average_distance))
-            f.write("Distance weighted travel_demand_d: {}\n".format(travel_demand / average_distance))
-            f.write('Distance weighted travel_demand_D_survey: {}\n'.format(travel_demand_D_survey / average_distance))
-            f.write("Distance weighted travel_demand_D_simulation: {}\n".format(travel_demand_D_simulation / average_distance))
+            f.write("Total popultion: {}\n".format(total_popultion))
+            f.write("Total flows: {}\n".format(flows))
+            f.write("Total travel_demand_d: {}\n".format(flows_d))
+            f.write('Total travel_demand_D_survey: {}\n'.format(flows_D_survey))
+            f.write("Total travel_demand_D_simulation: {}\n".format(flows_D_simulation))
+            f.write("Flows per people: {}\n".format(flows_p))
+            f.write("Travel_demand_d per people: {}\n".format(flows_d_p))
+            f.write("Travel_demand_D_survey per people: {}\n".format(flows_D_survey_p))
+            f.write("Travel_demand_D_simulation per people: {}\n".format(flows_D_simulation_p))
     
+        del flows
+        del flows_d
+        del flows_D_survey 
+        del flows_D_simulation
+        gc.collect()
         print("computing {} has finished! Total time: {} s.".format(name, time.time() - startTime))
-        
+
+    print(target_ssp.format("finish!"))
+    total_result = target_ssp.format("result")
+    result_path = '../results/{}.{}'.format(total_result, 'txt')
+    with open(result_path, 'w') as f:
+        f.write(total_result)
+        f.write('\n')
+        f.write('Total popultion: [{},{},{},{},{},{},{},{},{},{}]\n'.format(poplution[0], poplution[1], poplution[2], poplution[3], poplution[4], poplution[5], poplution[6], poplution[7], poplution[8], poplution[9]))
+
+        f.write('Total flows: [{},{},{},{},{},{},{},{},{},{}]\n'.format(flows[0], flows[1], flows[2], flows[3], flows[4], flows[5], flows[6], flows[7], flows[8], flows[9]))
+
+        f.write('Total travel_demand_d: [{},{},{},{},{},{},{},{},{},{}]\n'.format(flows_d[0], flows_d[1], flows_d[2], flows_d[3], flows_d[4], flows_d[5], flows_d[6], flows_d[7], flows_d[8], flows_d[9]))
+
+        f.write('Total travel_demand_D_survey: [{},{},{},{},{},{},{},{},{},{}]\n'.format(flows_D_survey[0], flows_D_survey[1], flows_D_survey[2], flows_D_survey[3], flows_D_survey[4], flows_D_survey[5], flows_D_survey[6], flows_D_survey[7], flows_D_survey[8], flows_D_survey[9]))
+
+        f.write('Total travel_demand_D_simulation: [{},{},{},{},{},{},{},{},{},{}]\n'.format(flows_D_simulation[0], flows_D_simulation[1], flows_D_simulation[2], flows_D_simulation[3], flows_D_simulation[4], flows_D_simulation[5], flows_D_simulation[6], flows_D_simulation[7], flows_D_simulation[8], flows_D_simulation[9]))
+
+        f.write('Flows per people: [{},{},{},{},{},{},{},{},{},{}]\n'.format(flows_p[0], flows_p[1], flows_p[2], flows_p[3], flows_p[4], flows_p[5], flows_p[6], flows_p[7], flows_p[8], flows_p[9]))
+
+        f.write('Travel_demand_d per people: [{},{},{},{},{},{},{},{},{},{}]\n'.format(flows_d_p[0], flows_d_p[1], flows_d_p[2], flows_d_p[3], flows_d_p[4], flows_d_p[5], flows_d_p[6], flows_d_p[7], flows_d_p[8], flows_d_p[9]))
+
+        f.write('Travel_demand_D_survey per people: [{},{},{},{},{},{},{},{},{},{}]\n'.format(flows_D_survey_p[0], flows_D_survey_p[1], flows_D_survey_p[2], flows_D_survey_p[3], flows_D_survey_p[4], flows_D_survey_p[5], flows_D_survey_p[6], flows_D_survey_p[7], flows_D_survey_p[8], flows_D_survey_p[9]))
+
+        f.write('Travel_demand_D_simulation per people: [{},{},{},{},{},{},{},{},{},{}]\n'.format(flows_D_simulation_p[0], flows_D_simulation_p[1], flows_D_simulation_p[2], flows_D_simulation_p[3], flows_D_simulation_p[4], flows_D_simulation_p[5], flows_D_simulation_p[6], flows_D_simulation_p[7], flows_D_simulation_p[8], flows_D_simulation_p[9]))
